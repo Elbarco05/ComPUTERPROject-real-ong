97464a979603702f168b8e400762bfccbfc7cb0d
Lowe Berg <loweberg04@gmail.com>
2026-01-28 11:30:03 +0100

    AAAA

--- task13.py
+++ task13.py
@@ -12,18 +12,29 @@ Z = 1
 
 Elis = []
 Logis = []
+GridPoints = []
+R = []
+PP = []
+
+fig, axs = plt.subplots(np.size(n))
 
 for n_ in n:
-    r, P, E = rad.radial(l, n_, Z)
-    rl, Pl, El = radlog.radiallog(l, n_, Z)
+    r, P, E = rad.radial(l, n_, Z, False)
+    rl, Pl, El, Gl = radlog.radiallog(l, n_, Z, False)
     Elis.append(E)
     Logis.append(El)
+    GridPoints.append(Gl)
+    R.append(rl)
 
 Elis = np.array(Elis)
 Logis = np.array(Logis)
 
 Realis = -(Z**2)/(2*np.power(n,2))
 
+axs[0].plot()
+
 print(Elis)
 print(Logis)
 print(Realis)
+
+print(GridPoints)
